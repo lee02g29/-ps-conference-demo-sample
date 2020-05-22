385f0440f1157082bbe777fd7e1cb666e00dec7a
lee02g29 <lee02g29@naver.com>
2020-05-22 13:52:53 +0900

    generate project

---
+++ src/main/java/com/puralsight/conferencedemo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.puralsight.conferencedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/puralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -0,0 +1,20 @@
+package com.puralsight.conferencedemo.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PersistenceConfiguration {
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder builder = DataSourceBuilder.create();
+        builder.url("jdbc:postgresql://localhost:5432/postgres");
+        builder.username("postgres");
+        builder.password("Welcome");
+        System.out.println("My Custom datasource bean has been initialized and set");
+        return builder.build();
+    }
+}
